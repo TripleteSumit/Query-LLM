4ade15285d9df43e777a229e8a1da9832746cc08
Sumit <91695958+TripleteSumit@users.noreply.github.com>
2024-01-27 19:19:45 +0530

    Did a lot of changes 1. Added the api key in .env file 2. Added one more url pattern for home 3. Created view for generating the responses of the user query 4. Showed the responses to the html page inside the text container 5. Added a gen.py file which handle to genrate the querys responses by the help of LLM 6. Added a config.yaml file to configure the LLM model with the framework 7. While Generating the responses it create a db file in the directory to store the sources of the data

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import query
+from .views import query, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
     path('chat/', query, name="user_query"),
 ]
