c73c4ae465dac21cf8c3693d64700784cdeb33b8
Sumit <91695958+TripleteSumit@users.noreply.github.com>
2024-02-06 23:34:51 +0530

    Added gunicorn server and whitenoise to server the static files, Added the all the dependency to settings.py and update the requirements.txt file

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -114,12 +115,14 @@ USE_I18N = True
 
 USE_TZ = True
 
+ALLOWED_HOSTS = [".onrender.com"]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
